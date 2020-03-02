c9d3c534cf37a775af897984f77ac46c5ec7a915
alexpaul <alexpaulnyc@gmail.com>
2020-03-02 18:23:32 -0500

    added show alert extension

---
+++ Firebase-Demo/Controllers/UIViewController+Alerts.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Alerts.swift
+//  Firebase-Demo
+//
+//  Created by Alex Paul on 3/2/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  public func showAlert(title: String?, message: String?) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+    alertController.addAction(okAction)
+    present(alertController, animated: true)
+  }
+}
